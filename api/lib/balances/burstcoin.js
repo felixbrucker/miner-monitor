901a58d1015946c56a7dc73e68ebaebd628510c7
Felix Brucker <felix.brucker@live.de>
2017-08-02 22:47:20 +0200

    Use burst wallet port instead of pool port

--- api/lib/balances/burstcoin.js
+++ api/lib/balances/burstcoin.js
@@ -5,6 +5,6 @@ module.exports = async (address) => {
   const agent = new https.Agent({
     rejectUnauthorized: false
   });
-  const balanceData = await axios.get(`http://burst.btfg.space:8124/burst?requestType=getAccount&account=${address}`, {httpsAgent: agent});
+  const balanceData = await axios.get(`http://burst.btfg.space:8125/burst?requestType=getAccount&account=${address}`, {httpsAgent: agent});
   return balanceData.data.effectiveBalanceNXT / 100000000;
 };
