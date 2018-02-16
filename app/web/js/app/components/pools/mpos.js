65cfd1484693a19822faaecd6254ff698b687c37
Felix Brucker <felix@felixbrucker.com>
2018-02-16 03:46:03 +0100

    Merge MPOS Pools

--- app/web/js/app/components/pools/mpos.js
+++ app/web/js/app/components/pools/mpos.js
@@ -1,16 +1,21 @@
 function MposController() {
   var ctrl = this;
+  ctrl.hidden = {};
 
-  ctrl.parseName = function(name) {
-    return (isNaN(name.charAt(0)) ? name : name.substr(1));
+  ctrl.toggleHidden = function(id) {
+    ctrl.hidden[id] = !ctrl.hidden[id];
   };
+
+  ctrl.isHidden = function(id) {
+    return ctrl.hidden[id];
+  }
 }
 
 angular.module('app').component('mpos', {
   templateUrl: 'views/partials/components/pools/mpos.html',
   controller: MposController,
   bindings: {
-    dashboard: '<',
+    dashboards: '<',
   }
 });
 
