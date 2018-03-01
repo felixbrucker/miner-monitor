513a58e22a947d2f95c794ab2d03aa858a81f85a
Felix Brucker <felix@felixbrucker.com>
2018-03-01 21:18:09 +0100

    Update syncProgress breakpoint

--- app/web/js/app/components/wallets/wallet.js
+++ app/web/js/app/components/wallets/wallet.js
@@ -2,11 +2,11 @@ function WalletController() {
   var ctrl = this;
 
   ctrl.isSynced = (data) => {
-    return moment().diff(moment.unix(data.lastBlockReceived), 'minutes') < 30 && (data.syncProgress ? data.syncProgress >= 1 : true);
+    return moment().diff(moment.unix(data.lastBlockReceived), 'minutes') < 30 && (data.syncProgress ? data.syncProgress >= 0.9999 : true);
   };
 
   ctrl.isSyncing = (data) => {
-    return (data.syncProgress ? data.syncProgress < 1 : false);
+    return (data.syncProgress ? data.syncProgress < 0.9999 : false);
   };
 
   ctrl.isOutdated = (data) => {
