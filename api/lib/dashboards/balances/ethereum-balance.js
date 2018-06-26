774669c64056e42d51e27355b65bf4a178306fbb
Felix Brucker <felix@felixbrucker.com>
2018-06-26 14:24:45 +0200

    Add error handling for ethplorer

--- api/lib/dashboards/balances/ethereum-balance.js
+++ api/lib/dashboards/balances/ethereum-balance.js
@@ -23,22 +23,20 @@ module.exports = class EthereumBalance extends Dashboard {
       const balanceData = await util.getUrl(`https://api.ethplorer.io/getAddressInfo/${this.dashboard.address}?apiKey=freekey`);
       const result = {
         eth: balanceData.ETH,
-        tokens: balanceData.tokens,
+        tokens: balanceData.tokens || [],
       };
       result.eth.balance = result.eth.balance || 0;
       const rate = util.getRateForTicker(this.coinmarketcap.getRates(), 'ETH');
       if (rate) {
         result.eth.balanceFiat = parseFloat(util.getFiatForRate(rate, this.coinmarketcap.getCurrency())) * result.eth.balance;
       }
-      if (result.tokens) {
-        result.tokens.forEach((token) => {
-          token.balance = token.balance / (Math.pow(10, parseInt(token.tokenInfo.decimals)));
-          const rate = util.getRateForTicker(this.coinmarketcap.getRates(), token.tokenInfo.symbol.toUpperCase());
-          if (rate) {
-            token.balanceFiat = parseFloat(util.getFiatForRate(rate, this.coinmarketcap.getCurrency())) * token.balance;
-          }
-        });
-      }
+      result.tokens.forEach((token) => {
+        token.balance = token.balance / (Math.pow(10, parseInt(token.tokenInfo.decimals)));
+        const rate = util.getRateForTicker(this.coinmarketcap.getRates(), token.tokenInfo.symbol.toUpperCase());
+        if (rate) {
+          token.balanceFiat = parseFloat(util.getFiatForRate(rate, this.coinmarketcap.getCurrency())) * token.balance;
+        }
+      });
 
       this.stats = result;
     } catch(err) {
