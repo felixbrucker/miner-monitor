e617d62d51ebe9fd25a4e34963c1002613b12ce1
Felix Brucker <felix@felixbrucker.com>
2018-10-22 15:29:20 +0200

    Update total plot size every hour

--- api/lib/miner/creep-miner.js
+++ api/lib/miner/creep-miner.js
@@ -2,6 +2,8 @@ const WebSocket = require('ws');
 const ReconnectingWebSocket  = require('reconnecting-websocket');
 const Miner = require('./miner');
 
+const reconnectInterval = 60 * 60 * 1000; // 1 hr
+
 module.exports = class CreepMiner extends Miner {
 
   getStats() {
@@ -13,39 +15,43 @@ module.exports = class CreepMiner extends Miner {
       WebSocket,
     });
 
-    this.client.addEventListener('message', (message) => {
-      const data = message.data;
-      if (!data) {
-        return;
-      }
-      if (data === 'ping') {
-        return;
-      }
-      const res = JSON.parse(data);
-      switch (res.type) {
-        case 'new block':
-          this.stats.block = res.block;
-          this.stats.blockStart = parseInt(res.startTime, 10);
-          this.stats.blocksWon = parseInt(res.blocksWon, 10);
-          this.stats.roundsSubmitted = parseInt(res.nRoundsSubmitted, 10);
-          this.stats.bestDL = null;
-          break;
-        case 'nonce found':
-        case 'nonce found (too high)':
-          const dl = parseInt(res.deadlineNum, 10);
-          if (!this.stats.bestDL || dl < this.stats.bestDL) {
-            this.stats.bestDL = dl;
-          }
-          break;
-        case 'nonce confirmed':
-        case 'nonce submitted':
-          break;
-        case 'config':
-          this.stats.totalPlotSize = res.totalPlotSize;
-          break;
-        default:
-          break;
-      }
-    });
+    this.client.addEventListener('message', this.onMessage.bind(this));
+    // Update the total plotSize regularly
+    setInterval(() => this.client.reconnect(), reconnectInterval);
+  }
+
+  onMessage(message) {
+    const data = message.data;
+    if (!data) {
+      return;
+    }
+    if (data === 'ping') {
+      return;
+    }
+    const res = JSON.parse(data);
+    switch (res.type) {
+      case 'new block':
+        this.stats.block = res.block;
+        this.stats.blockStart = parseInt(res.startTime, 10);
+        this.stats.blocksWon = parseInt(res.blocksWon, 10);
+        this.stats.roundsSubmitted = parseInt(res.nRoundsSubmitted, 10);
+        this.stats.bestDL = null;
+        break;
+      case 'nonce found':
+      case 'nonce found (too high)':
+        const dl = parseInt(res.deadlineNum, 10);
+        if (!this.stats.bestDL || dl < this.stats.bestDL) {
+          this.stats.bestDL = dl;
+        }
+        break;
+      case 'nonce confirmed':
+      case 'nonce submitted':
+        break;
+      case 'config':
+        this.stats.totalPlotSize = res.totalPlotSize;
+        break;
+      default:
+        break;
+    }
   }
 };
