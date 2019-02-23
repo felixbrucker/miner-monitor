0770588370d0a75e27e71c7f7df4d81fc5fdc0e2
Felix Brucker <felix@felixbrucker.com>
2019-02-23 22:15:17 +0100

    Use hdpool-api package

--- api/lib/dashboards/pool/hdpool.js
+++ api/lib/dashboards/pool/hdpool.js
@@ -1,5 +1,5 @@
-const ReconnectingWebSocket = require('reconnecting-websocket');
-const WebSocket = require('ws');
+const { HDPoolAccountApi } = require('hdpool-api');
+const moment = require('moment');
 const bytes = require('bytes');
 const Dashboard = require('../dashboard');
 const coinGecko = require('../../rates/coingecko');
@@ -26,157 +26,61 @@ module.exports = class HDPool extends Dashboard {
   }
 
   async onInit() {
-    this.client = new ReconnectingWebSocket(`${baseUrl}/?uid=${this.dashboard.user_id}&key=${this.dashboard.api_key}`, [], {
-      WebSocket,
-    });
-    this.client.addEventListener('message', this.onMessage.bind(this));
-
-    await new Promise(resolve => {
-      this.client.addEventListener('open', resolve, {once: true});
-    });
-
-    this.sendHeartbeat();
-    setInterval(this.sendHeartbeat.bind(this), 5 * 1000);
+    this.client = new HDPoolAccountApi(this.dashboard.user_id, this.dashboard.api_key);
+    await this.client.init();
 
     super.onInit();
   }
 
-  onMessage(msg) {
-    msg = JSON.parse(msg.data);
-    let rate = null;
-    switch (msg.cmd) {
-      case 'online.heartbeat':
-        break;
-      case 'apid.best_mining_info':
-        break;
-      case 'logind.get_uinfo':
-        if (!this.stats.lastPayedTs) {
-          this.stats.lastPayedTs = msg.para.wallet.update_ts;
-        }
-        this.stats.balance = msg.para.wallet.balance / Math.pow(10, 8);
-        rate = coinGecko.getRates('BHD').find(rate => rate.id === 'bitcoin-hd');
-        if (rate) {
-          this.stats.balanceFiat = parseFloat(rate.current_price) * this.stats.balance;
-        }
-        break;
-      case 'apid.get_index_data':
-        this.stats.incomeLastDay = msg.para.last24_income / Math.pow(10, 8);
-        this.stats.totalIncome = msg.para.total_income / Math.pow(10, 8);
-        this.stats.onlineCapacityString = bytes(bytes(`${msg.para.online_capacity}GB`));
-        rate = coinGecko.getRates('BHD').find(rate => rate.id === 'bitcoin-hd');
-        if (rate) {
-          this.stats.incomeLastDayFiat = parseFloat(rate.current_price) * this.stats.incomeLastDay;
-          this.stats.totalIncomeFiat = parseFloat(rate.current_price) * this.stats.totalIncome;
-        }
-        break;
-      case 'apid.get_mill_list':
-        this.stats.miners = msg.para.data.map(miner => ({
-          name: miner.name,
-          capacityString: bytes(bytes(`${miner.capacity}GB`)),
-          online: miner.is_online === 1,
-          lastSeen: new Date(miner.ts * 1000),
-        })).sort((a, b) => {
-          if (a.name < b.name) {
-            return -1;
-          }
-          if (a.name > b.name) {
-            return 1;
-          }
-          return 0;
-        });
-        break;
-      case 'apid.get_expected_profit':
-        this.stats.expectedProfit = msg.para.data.filter(round => round.create_ts > this.stats.lastPayedTs).reduce((acc, curr) => acc + (curr.curamt / Math.pow(10, 8)), 0);
-        rate = coinGecko.getRates('BHD').find(rate => rate.id === 'bitcoin-hd');
-        if (rate) {
-          this.stats.expectedProfitFiat = parseFloat(rate.current_price) * this.stats.expectedProfit;
-        }
-        break;
-      case 'apid.get_award_list':
-        const awardList = msg.para.data;
-        if (awardList.length === 0) {
-          break;
-        }
-        this.stats.lastPayedTs = awardList[0].create_ts;
-        break;
-      default:
-        console.log(`[${this.dashboard.name} :: HDPool-API] => Unknown cmd received: ${msg.cmd}. The following data was sent: ${JSON.stringify(msg)}`);
+  async updateStats() {
+    const userInfo = await this.client.getUserInfo();
+    const generalStats = await this.client.getGeneralStats();
+    const miners = await this.client.getMiners();
+    const expectedEarningsHistory = await this.client.getExpectedEarningsHistory();
+    const earningsHistory = await this.client.getEarningsHistory();
+
+    if (!this.stats.lastPayedTs) {
+      this.stats.lastPayedTs = userInfo.wallet.update_ts;
     }
-  }
+    this.stats.balance = userInfo.wallet.balance / Math.pow(10, 8);
+    this.stats.incomeLastDay = generalStats.last24_income / Math.pow(10, 8);
+    this.stats.totalIncome = generalStats.total_income / Math.pow(10, 8);
+    this.stats.onlineCapacityString = bytes(bytes(`${generalStats.online_capacity}GB`));
+    this.stats.miners = miners.map(miner => ({
+      name: miner.name,
+      capacityString: bytes(bytes(`${miner.capacity}GB`)),
+      online: moment().diff(miner.ts * 1000, 'minutes') < 5,
+      lastSeen: new Date(miner.ts * 1000),
+    })).sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    if (expectedEarningsHistory.data) {
+      this.stats.expectedProfit = expectedEarningsHistory.data
+        .filter(round => round.create_ts > this.stats.lastPayedTs)
+        .reduce((acc, curr) => acc + (curr.curamt / Math.pow(10, 8)), 0);
+    }
+    if (earningsHistory.data && earningsHistory.data.length !== 0) {
+      this.stats.lastPayedTs = earningsHistory.data[0].create_ts;
+    }
 
-  updateStats() {
-    this.requestUserInfo();
-    this.requestGeneralStats();
-    this.requestMiners();
-    this.requestAwardList();
-    this.requestExpectedProfit();
-  }
-
-  sendHeartbeat() {
-    this.client.send(JSON.stringify({
-      cmd: 'online.heartbeat',
-    }));
-  }
-
-  requestUserInfo() {
-    this.client.send(JSON.stringify({
-      chk: new Date().getTime(),
-      cmd: 'logind.get_uinfo',
-      para: {
-        uid: this.dashboard.user_id,
-      },
-    }));
-  }
-
-  requestGeneralStats() {
-    this.client.send(JSON.stringify({
-      chk: new Date().getTime(),
-      cmd: 'apid.get_index_data',
-      para: {
-        uid: this.dashboard.user_id,
-        type: 'bhd',
-      },
-    }));
-  }
-
-  requestMiners() {
-    this.client.send(JSON.stringify({
-      chk: new Date().getTime(),
-      cmd: 'apid.get_mill_list',
-      para: {
-        uid: this.dashboard.user_id,
-        type: 'bhd',
-      },
-    }));
-  }
-  requestExpectedProfit() {
-    this.client.send(JSON.stringify({
-      chk: new Date().getTime(),
-      cmd: 'apid.get_expected_profit',
-      para: {
-        uid: this.dashboard.user_id,
-        type: 'bhd',
-        offset: 0,
-        count: 200,
-      },
-    }));
-  }
-
-  requestAwardList() {
-    this.client.send(JSON.stringify({
-      chk: new Date().getTime(),
-      cmd: 'apid.get_award_list',
-      para: {
-        uid: this.dashboard.user_id,
-        type: 'bhd',
-        offset: 0,
-        count: 10,
-      },
-    }));
+    const rate = coinGecko.getRates('BHD').find(rate => rate.id === 'bitcoin-hd');
+    if (!rate) {
+      return;
+    }
+    this.stats.balanceFiat = parseFloat(rate.current_price) * this.stats.balance;
+    this.stats.incomeLastDayFiat = parseFloat(rate.current_price) * this.stats.incomeLastDay;
+    this.stats.totalIncomeFiat = parseFloat(rate.current_price) * this.stats.totalIncome;
+    this.stats.expectedProfitFiat = parseFloat(rate.current_price) * this.stats.expectedProfit;
   }
 
   cleanup() {
-    this.client.close();
+    this.client.destroy();
     super.cleanup();
   }
 };
