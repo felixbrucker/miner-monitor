8f7a86f3907744b76f8b8e998fdb6f40feb26df5
Felix Brucker <felix@felixbrucker.com>
2020-04-15 00:17:53 +0200

    Use storj dashboard http api

---
+++ api/lib/miner/storj.js
@@ -0,0 +1,99 @@
+const axios = require('axios');
+const moment = require('moment');
+const Miner = require('./miner');
+
+module.exports = class Storj extends Miner {
+  onInit() {
+    this.client = axios.create({
+      baseURL: `${this.device.hostname}/api/sno`,
+    });
+    this.historicalBandwidth = [];
+    this.ingressSpeed = 0;
+    this.egressSpeed = 0;
+    this.updateHistoricalbandwidthInterval = setInterval(this.updateHistoricalBandwidth.bind(this), 60 * 1000);
+    super.onInit();
+  }
+
+  async updateStats() {
+    try {
+      const { data: stats } = await this.client.get('/');
+      await Promise.all(stats.satellites.map(async satellite => {
+        const { data: perSatelliteStats } = await this.client.get(`/satellite/${satellite.id}`);
+        satellite.stats = {
+          storageSummary: perSatelliteStats.storageSummary,
+          bandwidthSummary: perSatelliteStats.bandwidthSummary,
+          egressSummary: perSatelliteStats.egressSummary,
+          ingressSummary: perSatelliteStats.ingressSummary,
+          vettingProgress: perSatelliteStats.audit.successCount < 100 ? perSatelliteStats.audit.successCount / 100 : 1,
+          auditScore: perSatelliteStats.audit.totalCount ? perSatelliteStats.audit.successCount / perSatelliteStats.audit.totalCount : 1,
+          uptimeScore: perSatelliteStats.uptime.totalCount ? perSatelliteStats.uptime.successCount / perSatelliteStats.uptime.totalCount : 1,
+        };
+        if (perSatelliteStats.audit.totalCount === 0) {
+          satellite.stats.vettingProgress = 1;
+        }
+      }));
+      const { data: satelliteStats } = await this.client.get('/satellites');
+      stats.egressSummary = satelliteStats.egressSummary;
+      stats.ingressSummary = satelliteStats.ingressSummary;
+
+      stats.uptime = Math.floor((Date.now() - (new Date(stats.startedAt)).getTime()) / 1000);
+
+      this.stats = stats;
+    } catch (err) {
+      this.stats = null;
+      console.error(`[${this.device.name} :: Storj] => ${err.message}`);
+    }
+  }
+
+  updateHistoricalBandwidth() {
+    if (!this.stats) {
+      return;
+    }
+    const newBandwidthEntry = [new Date(), this.stats.ingressSummary, this.stats.egressSummary];
+    this.historicalBandwidth.push(newBandwidthEntry);
+    this.historicalBandwidth = this.historicalBandwidth.filter(([entryTS]) => moment().diff(entryTS, 'minute') < 15);
+    this.updateBandwidthSpeeds();
+  }
+
+  updateBandwidthSpeeds() {
+    const oldestBandwidthEntry = this.historicalBandwidth.length === 0
+      ? [new Date(), this.stats.ingressSummary, this.stats.egressSummary]
+      : this.historicalBandwidth[0];
+    let timeInSecondsSince = moment().diff(oldestBandwidthEntry[0], 'second');
+
+    const differences = [this.stats.ingressSummary - oldestBandwidthEntry[1], this.stats.egressSummary - oldestBandwidthEntry[2]];
+    if (differences[0] < 0) {
+      differences[0] = this.stats.ingressSummary;
+      timeInSecondsSince = moment().diff(moment().startOf('day'), 'second');
+    }
+    if (differences[1] < 0) {
+      differences[1] = this.stats.egressSummary;
+      timeInSecondsSince = moment().diff(moment().startOf('day'), 'second');
+    }
+
+    this.ingressSpeed = differences[0] / timeInSecondsSince;
+    this.egressSpeed = differences[1] / timeInSecondsSince;
+  }
+
+  getStats() {
+    return Object.assign(
+      super.getStats(),
+      {
+        stats: this.stats,
+        id: this.device.id,
+      }, {
+        ingressSpeed: this.ingressSpeed,
+        egressSpeed: this.egressSpeed,
+      });
+  }
+
+
+
+  cleanup() {
+    if (this.updateHistoricalbandwidthInterval) {
+      clearInterval(this.updateHistoricalbandwidthInterval);
+      this.updateHistoricalbandwidthInterval = null;
+    }
+    super.cleanup();
+  }
+};
--- app/web/js/app/components/miner/storj.js
+++ app/web/js/app/components/miner/storj.js
@@ -10,15 +10,50 @@ function StorjController($http) {
     return (Date.now() - date) / 1000;
   };
 
-  ctrl.getStatus = function(lastContacted) {
-    if (!lastContacted) {
+  ctrl.getStatus = function() {
+    const stats = ctrl.entry.stats;
+    if (!stats.lastPinged) {
       return 0;
     }
-    if (moment(lastContacted).isBefore(moment().subtract(10, 'minute'))) {
+    if (moment(stats.lastPinged).isBefore(moment().subtract(10, 'minute'))) {
       return 1;
     }
+    const satellitesWithLowAuditScore = stats.satellites.filter(satellite => satellite.stats.auditScore <= 0.95);
+    const satellitesWithLowUptimeScore = stats.satellites.filter(satellite => satellite.stats.uptimeScore <= 0.9);
+    const disqualifiedSatellites = stats.satellites.filter(satellite => satellite.disqualified);
+    if (satellitesWithLowAuditScore.length > 0 || satellitesWithLowUptimeScore.length > 0 || disqualifiedSatellites.length > 0) {
+      return 2;
+    }
+    const satellitesCurrentlyVetting = stats.satellites.filter(satellite => satellite.stats.vettingProgress < 1);
+    if (satellitesCurrentlyVetting.length > 0) {
+      return 3;
+    }
 
-    return 2;
+    return 4;
+  };
+
+  ctrl.getMessages = () => {
+    const stats = ctrl.entry.stats;
+
+    const allMessages = stats.satellites.map(satellite => {
+      const messages = [];
+      if (satellite.stats.auditScore <= 0.95) {
+        messages.push(`${satellite.url.replace(':7777', '')} | Low audit score: ${(satellite.stats.auditScore * 100).toFixed(1)}%`);
+      }
+      if (satellite.stats.uptimeScore <= 0.9) {
+        messages.push(`${satellite.url.replace(':7777', '')} | Low uptime score: ${(satellite.stats.uptimeScore * 100).toFixed(1)}%`);
+      }
+      if (satellite.stats.vettingProgress < 1) {
+        messages.push(`${satellite.url.replace(':7777', '')} | Vetting progress: ${(satellite.stats.vettingProgress * 100).toFixed(0)}%`);
+      }
+      if (satellite.disqualified) {
+        messages.push(`${satellite.url.replace(':7777', '')} | Node is disqualified`);
+      }
+
+      return messages;
+    });
+
+    return allMessages.reduce((acc, curr) => acc.concat(curr), []);
   };
 }
 
