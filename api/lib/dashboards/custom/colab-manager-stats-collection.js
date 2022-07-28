358ea5be930bbc4c959688c58218b889b248ca92
Felix Brucker <felix@felixbrucker.com>
2022-07-29 00:27:00 +0200

    Sort by doc name

--- api/lib/dashboards/custom/colab-manager-stats-collection.js
+++ api/lib/dashboards/custom/colab-manager-stats-collection.js
@@ -21,13 +21,24 @@ module.exports = class ColabManagerStatsCollection extends Dashboard {
     try {
       const { data } = await this.apiClient.get(`/api/stats`)
 
-      this.stats = data
+      const workers = data
         .map(manager => manager.workers.map(worker => ({
           managerId: manager.id,
           workerId: worker.id,
           stats: worker.stats,
         })))
         .reduce((acc, curr) => acc.concat(curr), [])
+      workers.sort((a, b) => {
+        if (a.stats.documentName < b.stats.documentName) {
+          return -1
+        }
+        if (a.stats.documentName > b.stats.documentName) {
+          return 1
+        }
+
+        return 0
+      })
+      this.stats = workers
     } catch(err) {
       console.error(`[${this.dashboard.name} :: Colab-Manager-Stats-Collection] => ${err.message}`)
     }
